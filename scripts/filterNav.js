13afebb2a8fa4fd4aca4269ec9fa08a4783939c2
p.horovenko <p.horovenko@LINKER-W3>
2022-04-13 11:04:32 +0300

    remove old code

--- scripts/filterNav.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px */
-function openFilterNav() {
-    document.getElementById("myFilterNav").style.width = "250px";    
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeFilterNav() {
-    document.getElementById("myFilterNav").style.width = "0";
-}
