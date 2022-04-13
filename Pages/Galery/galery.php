274fe2b489a38432441894d760ef8f51661a359b
watermelonrytp@gmail.com <Felix>
2022-04-13 20:23:37 +0300

    mock pages

--- Pages/Galery/galery.php
+++ Pages/Galery/galery.php
@@ -3,41 +3,41 @@ include "../../Components/SearchUL/listElm.php";
 include "../../Components/Galery/galeryElement.php";
 
 $styles = <<<END
-    <link rel="stylesheet" href="../../Components/SearchUL/coolSearch.css">
-    <link rel="stylesheet" href="../../Components/Filter/filterNav.css">
-    <link rel="stylesheet" href="../../Components/Galery/styles.css">
+    <link rel="stylesheet" href="/Components/SearchUL/coolSearch.css">
+    <link rel="stylesheet" href="/Components/Filter/filterNav.css">
+    <link rel="stylesheet" href="/Components/Galery/styles.css">
 END;
 
 $jsModules = <<<END
-    <script src="../../Components/SearchUL/coolSearch.js"></script>
-    <script src="../../Components/Filter/filterNav.js"></script>
+    <script src="/Components/SearchUL/coolSearch.js"></script>
+    <script src="/Components/Filter/filterNav.js"></script>
 END;
 
-$baseTemplate = file_get_contents("../../Templates/main_template.html");
-$baseTemplate = str_replace($_SERVER['SCRIPT_NAME'].'">', $_SERVER['SCRIPT_NAME'].'" class="active">', $baseTemplate);
+$baseTemplate = file_get_contents($_SERVER['DOCUMENT_ROOT']."/Templates/main_template.html");
+$baseTemplate = str_replace($_SERVER['REQUEST_URI'].'">', $_SERVER['REQUEST_URI'].'" class="active">', $baseTemplate);
 
 $baseTemplate = str_replace('{{JSmodules}}', $jsModules, $baseTemplate);
 $baseTemplate = str_replace('{{Styles}}', $styles, $baseTemplate);
 
-$filter = file_get_contents("../../Components/SearchUL/template.html");
+$filter = file_get_contents($_SERVER['DOCUMENT_ROOT']."/Components/SearchUL/template.html");
 $list = array(
     (new listElm("#","Adele"))->toStr(),
     (new listElm("#","Adele"))->toStr()
 );
 $filter = str_replace('{{Values}}', join($list), $filter);
 
-$galery = file_get_contents("../../Components/Galery/galery.html");
+$galery = file_get_contents($_SERVER['DOCUMENT_ROOT']."/Components/Galery/galery.html");
 $list = array(
-        (new GaleryElement("jopa","","/assets/imgs/guitars/374850.jpg"))->print_name(),
-        (new GaleryElement("jopa","","/assets/imgs/guitars/374857.jpg"))->print_name(),
-        (new GaleryElement("sds","","/assets/imgs/guitars/374853.jpg"))->print_name(), 
-        (new GaleryElement("sds","","/assets/imgs/guitars/374850.jpg"))->print_name(),
-        (new GaleryElement("sds","","/assets/imgs/guitars/374853.jpg"))->print_name(),
-        (new GaleryElement("sds","","/assets/imgs/guitars/374857.jpg"))->print_name(),        
+        (new GaleryElement("Guitar 1","","374850"))->print_name(),
+        (new GaleryElement("Guitar 2","","374857"))->print_name(),
+        (new GaleryElement("Guitar 3","","374853"))->print_name(), 
+        (new GaleryElement("Guitar 4","","374850"))->print_name(),
+        (new GaleryElement("Guitar 5","","374853"))->print_name(),
+        (new GaleryElement("Guitar 6","","374857"))->print_name(),        
 );
 $galery = str_replace('{{Values}}', join($list), $galery);
 
-$filterNav = file_get_contents("../../Components/Filter/filterNav.html");
+$filterNav = file_get_contents($_SERVER['DOCUMENT_ROOT']."/Components/Filter/filterNav.html");
 
 
 
