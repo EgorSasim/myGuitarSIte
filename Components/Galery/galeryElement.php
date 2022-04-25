d30e6b9936866882fefde24bc8744bd2d2dd6e19
p.horovenko <p.horovenko@LINKER-W3>
2022-04-25 14:05:48 +0300

    Guitar API mock

--- Components/Galery/galeryElement.php
+++ Components/Galery/galeryElement.php
@@ -18,4 +18,33 @@ class GaleryElement
         return sprintf(self::$template, $this->name, $this->id);
     }
 }
+
+class GuitarJson{
+    public static $path = '/assets/imgs/guitars/%1$s.jpg';
+
+    public $name;
+    public $price;
+    public $picture;
+    public $id;
+
+    public function __construct($name, $price, $id) {
+        $this->price = $price;
+        $this->name = $name;
+        $this->id = $id;
+
+        $this->picture = sprintf(self::$path, $this->id);
+    }
+
+    public function jsonSerialize()
+    {
+        $arr = array(
+            'name' => $this->name,
+            'price' => $this->price,
+            'id' => $this->id,
+            'path' => $this->picture
+        );
+
+        return $arr;
+    }
+}
 ?>
