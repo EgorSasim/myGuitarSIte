7a5336687073f4bf2e3e52d30711d49284d69500
Gorgy <egorsasim@mail.ru>
2022-04-26 19:02:51 +0300

    add smth

--- scripts/filterNav.js
+++ scripts/filterNav.js
@@ -1,4 +1,33 @@
-/* Set the width of the side navigation to 250px */
+// Show appropriate fields when the certain type of guitar choosen
+let $GuitarType = document.querySelector('.guitarType');
+let $classicGitCharacteristics = document.querySelector('.classicGuitarScpecifications');
+let $electronicGitCharacteristics = document.querySelector('.electronicGuitarScpecifications');
+let $acousticGitCharacteristics = document.querySelector('.acousticGuitarScpecifications');
+
+$GuitarType.onchange =() => {
+  showAppropriateGuitarCharacteristics($GuitarType.selectedIndex);
+}
+
+function showAppropriateGuitarCharacteristics(gitType)
+{
+  $acousticGitCharacteristics.style.display = $electronicGitCharacteristics.style.display =  $classicGitCharacteristics.style.display = "none";
+  if(gitType == 1)
+    showGuitarCharacteristics($classicGitCharacteristics);
+  else if(gitType == 2)
+    showGuitarCharacteristics($electronicGitCharacteristics);
+  else if(gitType == 3)
+    showGuitarCharacteristics($acousticGitCharacteristics);
+}
+
+
+function showGuitarCharacteristics(gitCharacteristics){
+  gitCharacteristics.style.display = "block";
+}
+
+
+
+
+/* Set the width of the side navigation to 500px */
 function openFilterNav() {
     document.getElementById("myFilterNav").style.width = "500px";    
   }
